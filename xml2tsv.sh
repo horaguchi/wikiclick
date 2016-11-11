c31626b9f95a1a4ff721148674e9b4535208c2fe
horaguchi <koji.horaguchi@gmail.com>
2016-11-12 05:20:06 +0900

    Compressed the wikipedia dumps

--- xml2tsv.sh
+++ xml2tsv.sh
@@ -2,3 +2,5 @@
 
 cat jawiki-latest-abstract.xml | perl -lne 'if (m{^<title>(.*)</title>$}) { $t = $1; } elsif (m{^<url>(..*)</url>}) { $u = $1; } elsif (m{^<abstract>(.*)</abstract>$}) { $ab = $1; } else { $ab =~ s{[\[\]{}]}{}g; print "$t\t$u\t$ab" if $t and $u and $ab and $ab !~ m{[|]} and length($ab) > 64; $t = $u = $ab = ""; } ' > title_url_abstract_ja.tsv
 cat enwiki-latest-abstract.xml | perl -lne 'if (m{^<title>(.*)</title>$}) { $t = $1; } elsif (m{^<url>(..*)</url>}) { $u = $1; } elsif (m{^<abstract>(.*)</abstract>$}) { $ab = $1; } else { $ab =~ s{[\[\]{}]}{}g; print "$t\t$u\t$ab" if $t and $u and $ab and $ab !~ m{[|]} and length($ab) > 64; $t = $u = $ab = ""; } ' > title_url_abstract_en.tsv
+gzip -f title_url_abstract_ja.tsv
+gzip -f title_url_abstract_en.tsv
